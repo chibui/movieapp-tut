6f04e6ba12293413dc219c43ba2928ee909659e7
Chi Bui <Chi_bui@outlook.com>
2018-05-08 11:19:09 +1000

    replace omdb service

---
+++ src/omdb/service.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('omdb', [])
+        .factory('omdbApi', function ($http, $q) {
+            var service = {},
+                baseUrl = 'http://www.omdbapi.com/?v=1&',
+                apiKey = '&apikey=bd925466';
+
+            function httpPromise(url) {
+                var deferred = $q.defer();
+
+                $http.get(url)
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    })
+                    .error(function () {
+                        deferred.reject();
+                    });
+                return deferred.promise;
+            }
+
+            service.search = function (query) {
+                return httpPromise(baseUrl + 's=' + encodeURIComponent(query) + apiKey);
+            };
+
+            service.find = function (id) {
+                return httpPromise(baseUrl + 'i=' + id + apiKey);
+            };
+
+            return service;
+        });
+})();
