74b3bec29a1638a461b25a8d2c9b5d497334096a
Chi Bui <Chi_bui@outlook.com>
2018-05-09 11:58:44 +1000

    add rotating popular movies
    add home controller, view and tests

---
+++ src/movie-app/home.controller.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('movieApp')
+        .controller('HomeController', function ($scope, $interval, omdbApi, PopularMovies) {
+            var results = [],
+                index = 0,
+                findMovie = function(id) {
+                    omdbApi.find(id)
+                        .then(function (data) {
+                            $scope.result = data;
+                        })
+                };
+
+            // PopularMovies.get()
+            //     .then(function (data) {
+                    var data = ["tt0076759", "tt0080684", "tt0086190"];
+                    results = data;
+                    findMovie(results[0]);
+
+                    $interval(function () {
+                        ++index;
+                        findMovie(results[index % results.length]);
+                    }, 5000);
+
+                // });
+
+
+
+        });
+})();
